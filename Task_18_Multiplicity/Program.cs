15b292baf80c35e2a648ff1464349bc156789fbd
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-18 22:02:57 +0300

    #1

---
+++ Task_18_Multiplicity/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_18_Multiplicity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowlimit = 100;
+            int highlimit = 1000;
+            int numbersMultiples = 0;
+
+            Random random = new Random();
+            int N = random.Next(1, 27);
+
+            while (lowlimit != highlimit)
+            {
+                int temporarySum = 0;
+                for (int i = 0; i < lowlimit; i += N)
+                {
+                    temporarySum += N;
+                }
+
+                if (temporarySum == lowlimit)
+                    numbersMultiples++;
+
+                lowlimit++;
+            }
+
+            Console.WriteLine(numbersMultiples);
+        }
+    }
+}
